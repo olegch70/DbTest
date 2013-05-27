5f3955ebf3c2c4a3ecba1f77eb1855c1b3b93643
Chamlay Oleg <oleg_ch70@mail.ru>
2013-05-27 11:22:40 +0400

    work with Idea and AIDE

--- src/com/app/dbtest/MainActivity.java
+++ src/com/app/dbtest/MainActivity.java
@@ -1,17 +1,42 @@
 package com.app.dbtest;
 
-import android.app.*;
-import android.os.*;
-import android.view.*;
-import android.widget.*;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-	{
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
+        ((Button) findViewById(R.id.btFillDb)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DbFiller.fillDb(MainActivity.this, ((CheckBox) findViewById(R.id.cbClear)).isChecked());
+            }
+        });
+
+        ((Button) findViewById(R.id.btShowDbContent)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dbShowContentIntent = new Intent(MainActivity.this, DbContentActivity.class);
+                startActivity(dbShowContentIntent);
+            }
+        });
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+    
 }
